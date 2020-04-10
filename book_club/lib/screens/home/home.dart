663d55f5d1b4b531abcf3d3961940f530ddab343
Amateur Coder <connectwithamateur@gmail.com>
2020-04-09 19:25:02 -0500

    Update

--- book_club/lib/screens/home/home.dart
+++ book_club/lib/screens/home/home.dart
@@ -72,7 +72,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         child: Row(
                           children: <Widget>[
                             Text(
-                              "Due In: ",
+                              "Due On: ",
                               style: TextStyle(
                                 fontSize: 30,
                                 color: Colors.grey[600],
