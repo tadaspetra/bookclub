1ff000600695b392307d27c4c3da48a29652029c
Amateur Coder <connectwithamateur@gmail.com>
2020-07-30 16:24:46 -0500

    Updated with APNs

--- convene/ios/Runner/AppDelegate.swift
+++ convene/ios/Runner/AppDelegate.swift
@@ -9,6 +9,9 @@ import Firebase
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     FirebaseApp.configure()
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
